11eb00f9fa005b232f4faff67a838f62b335e9a6
amanysafwat <120970169+amanysafwat@users.noreply.github.com>
2022-12-21 05:19:13 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        int n, i, sum;
+        int n1, n2;
+
+        Console.Write("n1 = ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("n2 = ");
+        n2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The Perfect numbers within the given range : ");
+        for (n = n1; n <= n2; n++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == n)
+                Console.WriteLine("\t" + "{0} ", n);
+        }
+        Console.Write("\n");
+        Console.ReadLine();
+    }
+}
